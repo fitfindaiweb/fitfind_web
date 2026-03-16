6597a815996f8ca8c88c156c53dd72d58d2f1ab0
sarim-tafsol <sarim.js@tafsol.com>
2026-03-16 23:03:28 +0500

    live

--- src/config/apiUrl.js
+++ src/config/apiUrl.js
@@ -1,7 +1,4 @@
 export const apiUrl = "https://api.fitfind.app";
-
-// export const apiUrl = "https://7l60bxbl-4017.inc1.devtunnels.ms";
-// live 1
 export const s3Url = "https://fitfindbucket.s3.us-east-2.amazonaws.com";
 
 export const imageUrl = (url) => `${s3Url}/${url}`;
